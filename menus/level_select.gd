157f69c8f048c6bbc0b0f8e63bafb23b19478bcd
Michael <132649930+einbaumhaus@users.noreply.github.com>
2025-04-07 23:28:49 +0200

    new menus

--- menus/level_select.gd
+++ menus/level_select.gd
@@ -11,17 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_credits_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
-
-
-func _on_settings_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_play_pressed() -> void:
+func _on_back_b_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/mode_select.tscn")

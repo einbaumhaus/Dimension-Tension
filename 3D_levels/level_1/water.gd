f6462ffe5421e96438a65730a7e2a40001e71f8c
Michael <132649930+einbaumhaus@users.noreply.github.com>
2025-04-18 00:33:14 +0200

    new materials, added rails and env

---
+++ 3D_levels/level_1/water.gd
@@ -0,0 +1,12 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var mat = get_active_material(0)
+	mat.uv1_offset += Vector3(0,0.2,0)*delta

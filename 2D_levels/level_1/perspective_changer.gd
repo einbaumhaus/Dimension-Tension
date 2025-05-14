a8b2e58014586a3aeab8ce1db7cda03ae89ff662
Michael <132649930+einbaumhaus@users.noreply.github.com>
2025-05-14 21:53:17 +0200

    bug fixes

--- 2D_levels/level_1/perspective_changer.gd
+++ 2D_levels/level_1/perspective_changer.gd
@@ -11,6 +11,7 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_body_entered(body: CharacterBody2D) -> void:
-	print("entered")
-	get_tree().change_scene_to_file.call_deferred("res://3D_levels/level_1/level_1_3d.tscn")
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		print("entered")
+		get_tree().change_scene_to_file.call_deferred("res://3D_levels/level_1/level_1_3d.tscn")

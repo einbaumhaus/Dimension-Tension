b42b8cd80f6301a4b4567e714dd2a11b70f1a980
Michael <132649930+einbaumhaus@users.noreply.github.com>
2025-05-22 18:41:04 +0200

    lvl_1_2d collision fix

--- menus/mode_select.gd
+++ menus/mode_select.gd
@@ -17,3 +17,7 @@ func _on_level_select_b_pressed() -> void:
 
 func _on_back_b_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/main_menu.tscn")
+
+
+func _on_storymode_b_pressed() -> void:
+	get_tree().change_scene_to_file("res://mixed_levels/intro/intro_level.tscn")
--- mixed_levels/intro/intro_level.gd
+++ mixed_levels/intro/intro_level.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var animation_player: AnimationPlayer = $part1/part1player
 @onready var part_2_player: AnimationPlayer = $part2/part2player
+@onready var part_3_player: AnimationPlayer = $part3/part3player
+@onready var part_4_player: AnimationPlayer = $part1/part4player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +14,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_part_1_player_animation_finished(anim_name: StringName) -> void:
 	part_2_player.play("part2")
+
+
+func _on_part_2_player_animation_finished(anim_name: StringName) -> void:
+	part_3_player.play("part3")
+
+
+func _on_part_3_player_animation_finished(anim_name: StringName) -> void:
+	part_4_player.play("part4")

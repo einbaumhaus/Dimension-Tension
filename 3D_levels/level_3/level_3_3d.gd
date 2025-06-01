ea4a8e801d854ff4f8ddd928bd89d76c8a27b5d1
Michael <132649930+einbaumhaus@users.noreply.github.com>
2025-06-01 22:08:01 +0200

    removed real light from mitzi, replaced with texture

--- 3D_levels/level_3/level_3_3d.gd
+++ 3D_levels/level_3/level_3_3d.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	player_flashlight.visible = true
 
 
